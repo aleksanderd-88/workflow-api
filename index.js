cd3bd5e6596b928583886838e20327569cc78ef0
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-12-27 22:33:54 +0100

    Re-order middlewares

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const port = process.env.PORT || 1234
 const cors = require('cors')
 const routes = require('./src/routes')
 
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 
 app.use('/', routes)
 
